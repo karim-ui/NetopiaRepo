87f94299a1570ba7a17c5776b3a3a01fd45df126
abdelkarim.imghi <abdelkarim.imghi@gmail.com>
2021-05-17 10:02:11 +0100

    typemotif

---
+++ src/main/java/com/reclamationproject/reclamationproj/Models/Reclamation.java
@@ -0,0 +1,23 @@
+/*
+package com.reclamationproject.reclamationproj.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ref_reclamation")
+public class Reclamation {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long reclamation_id;
+    @Column(unique=true,length = 32)
+    private String idcs;
+    @Column(unique=true,length = 32)
+    private String motif_code;
+    private String objet;
+    private String description;
+    //private FileDB
+
+}
+*/
